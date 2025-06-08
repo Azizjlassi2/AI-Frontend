9808f9c8802b5ee060c87a690ec17be9bef26a7a
aziz jlassi <azizjlassi498@gmail.com>
2025-06-08 15:25:31 +0100

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/AIplus/',  // <-- Add this line
+  base: '/AI-Frontend/',  // <-- Add this line
   build: {
     chunkSizeWarningLimit: 1500,
   },
